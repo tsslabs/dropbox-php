7fd49095f914f07b2493766ec21ae62412a216d1
Radu Topala <radu.topala@trisoft.ro>
2016-02-26 13:41:23 +0000

    added shares options array

--- src/Dropbox/API.php
+++ src/Dropbox/API.php
@@ -337,7 +337,7 @@ class Dropbox_API {
         if (is_null($root)) $root = $this->root;
         $path = str_replace(array('%2F','~'), array('/','%7E'), rawurlencode($path));
         $response = $this->oauth->fetch($this->api_url.  'shares/'. $root . '/' . ltrim($path, '/'), $options, 'POST');
-        return json_decode($response['body'],true)
+        return json_decode($response['body'],true);
     }
 
     /**
